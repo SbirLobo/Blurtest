c729fbed0543f47fa192742894ed59d7feb52c6c
Pierre Lacoste <p.lacoste33000@gmail.com>
2023-04-13 16:30:01 +0200

    FOOTER version responsive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,10 +4,22 @@ function Footer() {
       <div className="absolute left-10">
         <img src="./src/assets/copyright.svg" alt="Copyright" className="h-2" />
       </div>
-      <div className="flex justify-center w-3/4 space-x-12  ">
-        <img src="./src/assets/facebook.svg" alt="Facebook" className="h-6" />
-        <img src="./src/assets/instagram.svg" alt="Instagram" className="h-6" />
-        <img src="./src/assets/twitter.svg" alt="Twitter" className="h-6" />
+      <div className="flex justify-center w-3/4 space-x-12 md:space-x-20 xl:gap-12">
+        <img
+          src="./src/assets/facebook.svg"
+          alt="Facebook"
+          className="h-4 md:h-6"
+        />
+        <img
+          src="./src/assets/instagram.svg"
+          alt="Instagram"
+          className="h-4 md:h-6"
+        />
+        <img
+          src="./src/assets/twitter.svg"
+          alt="Twitter"
+          className="h-4 md:h-6"
+        />
       </div>
     </footer>
   );
