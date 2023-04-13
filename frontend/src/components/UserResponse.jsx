c76ac1ae0c48634455d6a296189dbdcfe5b32a00
Chl0l <chloe.kim.elle@gmail.com>
2023-04-13 14:48:24 +0200

    Rename component

---
+++ frontend/src/components/UserResponse.jsx
@@ -0,0 +1,12 @@
+function UserResponse() {
+  return (
+    <input
+      type="text"
+      id="response"
+      name="response"
+      className="inputResponse"
+    />
+  );
+}
+
+export default UserResponse;
