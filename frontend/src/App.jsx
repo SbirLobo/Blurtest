d6004dbe5bbb4bedfb0116354fb5da04dcc28834
Kyxk <kylian.gronnier@gmail.com>
2023-04-14 11:39:39 +0200

    titre+affiche

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import CardFilm from "./components/CardFilm";
-import themes from "./data/film.json";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            {Object.keys(themes).map((theme) => (
-              <li key={theme}>
-                <Link to={`/${theme}`}>{theme}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="flex flex-col items-center justify-center content-center">
-          <div className="flex-0 flex-auto order-0 self-auto">
-            <Routes>
-              {Object.entries(themes).map((theme) => (
-                <Route
-                  key={theme[0]}
-                  path={`/${theme[0]}`}
-                  element={
-                    <CardFilm theme={theme[0]} img={theme[1].imageUrl} />
-                  }
-                />
-              ))}
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
+    <div>
+      <CardFilm />
+    </div>
   );
 }
 
--- frontend/src/components/CardFilm.jsx
+++ frontend/src/components/CardFilm.jsx
@@ -1,36 +1,12 @@
-// eslint-disable-next-line react/prop-types
-function CardFilm({ theme }) {
+function CardFilm() {
   return (
-    <div
-      // className="flex flex-col bg-primary items-center justify-start content-stretch"
-      style={{
-        backgroundColor: "rgb(38, 70, 83)",
-        display: "flex",
-        flexDirection: "column",
-        flexWrap: "nowrap",
-        alignContent: "space-around",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div className="order-1 flex-none">
-        <h3
-          style={{
-            backgroundColor: "#FAF9F0",
-            borderRadius: "8px",
-            padding: "8px 50px",
-            border: "solid 1px",
-          }}
-        >
-          {theme}
-        </h3>
-      </div>
-      <div className="order-1 flex-none">
-        <img
-          src="https://www.themoviedb.org/t/p/original/1P3ZyEq02wcTMd3iE4ebtLvncvH.jpg"
-          alt={theme}
-        />
-      </div>
+    <div className="cardfilm">
+      <h3 className="cardfilmTitle">theme</h3>
+
+      <img
+        src="https://www.themoviedb.org/t/p/original/1P3ZyEq02wcTMd3iE4ebtLvncvH.jpg"
+        alt="affiche film"
+      />
     </div>
   );
 }
