1ecf8d1103000e7aa29368aaa101adf6ad2d72e5
Vinch <vince.daviaud@gmail.com>
2023-04-14 22:32:00 +0200

    add img div for blur overflow hidden

--- frontend/src/components/CardFilm.jsx
+++ frontend/src/components/CardFilm.jsx
@@ -3,11 +3,13 @@ function CardFilm() {
     <div className="cardfilm md:mt-0">
       <h3 className="cardfilmTitle h-7 ">Theme</h3>
 
-      <img
-        className="w-52 mt-3 mb-2 rounded md:w-80 md:mt-10"
-        src="https://i0.wp.com/www.filmspourenfants.net/wp-content/uploads/2018/04/harry-potter-et-la-chambre-des-secrets-a.jpg?fit=555%2C786&ssl=1"
-        alt="affiche film"
-      />
+      <div className="mt-3 mb-2 overflow-hidden rounded">
+        <img
+          className="w-52 rounded md:w-80 md:mt-10"
+          src="https://i0.wp.com/www.filmspourenfants.net/wp-content/uploads/2018/04/harry-potter-et-la-chambre-des-secrets-a.jpg?fit=555%2C786&ssl=1"
+          alt="affiche film"
+        />
+      </div>
     </div>
   );
 }
