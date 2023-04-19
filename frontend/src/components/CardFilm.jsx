99f67203a1a9d311b90f91a2b2c2e46ad57976b6
Pierre Lacoste <p.lacoste33000@gmail.com>
2023-04-19 12:27:53 +0200

    modif Margin blur

--- frontend/src/components/CardFilm.jsx
+++ frontend/src/components/CardFilm.jsx
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 function CardFilm({ testarr }) {
   return (
     <div className="cardfilm md:mt-0">
-      <h3 className="cardfilmTitle h-7 md:text-2xl">Theme</h3>
+      <h3 className="cardfilmTitle h-7 md:text-2xl mb-10">Theme</h3>
 
-      <div className=" mt-3 mb-2 overflow-hidden rounded">
+      <div className=" mt-3 mb-2 overflow-hidden rounded ">
         <img
-          className=" affiche w-52 rounded md:w-80 md:mt-10"
+          className=" affiche w-52 rounded md:w-80 md:mt-0 "
           src={`https://image.tmdb.org/t/p/w500${testarr.poster_path}`}
           alt="affiche film"
         />
